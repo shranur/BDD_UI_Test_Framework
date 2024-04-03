82eb7e0d7f2ceb3342b3997b028097cc59ab20c9
Anurag Shrivastava <anuragshrivastava007@gmail.com>
2024-04-03 16:59:16 +0530

    pipeline changes

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,20 +1,12 @@
 package runner;
 
-
-
-
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+@CucumberOptions(features = "src/test/resources/features", glue = { "stepdefinitions",
+		"hooks" }, publish = true, plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })
 
-@CucumberOptions(features="src/test/resources/features",
-				 glue={"stepdefinitions","hooks"},
-				 publish=true,
-				 plugin={"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
-
-public class TestRunner extends AbstractTestNGCucumberTests{
-	
-	
+public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
 
